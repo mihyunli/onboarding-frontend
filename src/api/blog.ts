690d1e8520d50585b51d1478086c570031e23eac
mihyunli <mihyunlee@gitplecorp.com>
2022-06-14 18:02:12 +0900

    feat: 블로그 포스트 client API 추가

---
+++ src/api/blog.ts
@@ -0,0 +1,47 @@
+import httpClient from '../utils/axios';
+// ----------------------------------------------------------------------
+
+export const URLS = {
+  POST: `/blog/post`,
+  POSTS: `/blog/posts`,
+};
+
+interface IPostType {
+  id?: string;
+  title: string;
+  body: string;
+}
+
+export async function getAllPosts(): Promise<any> {
+  const { data } = await httpClient({
+    method: 'GET',
+    url: URLS.POSTS,
+  });
+  return data;
+}
+
+export async function createPost(form: IPostType): Promise<any> {
+  const { data } = await httpClient({
+    method: 'POST',
+    url: `${URLS.POST}/${form.id}`,
+    data: { ...form },
+  });
+  return data;
+}
+
+export async function updatePost(form: IPostType): Promise<any> {
+  const { data } = await httpClient({
+    method: 'PUT',
+    url: `${URLS.POST}/${form.id}`,
+    data: { ...form },
+  });
+  return data;
+}
+
+export async function deletePost(postId: String): Promise<any> {
+  const { data } = await httpClient({
+    method: 'DELETE',
+    url: `${URLS.POST}/${postId}`,
+  });
+  return data;
+}
