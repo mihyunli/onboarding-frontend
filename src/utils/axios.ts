e5c737088d6cfbf1d5f324c75c267f116d5ee80e
mihyunli <mihyunlee@gitplecorp.com>
2022-06-17 17:37:15 +0900

    chore: 워크스페이스 구성을 위해 상위 프로젝트 정리

--- src/utils/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-
-// ----------------------------------------------------------------------
-
-const axiosInstance: AxiosInstance = axios.create();
-
-export default axiosInstance;
